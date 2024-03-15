05098c4850587036eafed9765435def073980298
Stephanie-Castro-Dos-Santos <fp.daw.ies.ciudad.escolar@gmail.com>
2024-03-15 10:36:51 +0000

    Changed the text, added a picture and create new styles

---
+++ test_react_native/App.js
@@ -0,0 +1,38 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+ /*<View></View>*/
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={[styles2.text]}>Hello World! I'm Queen</Text>
+      {/*<Image
+        source={{
+          uri: 'https://images.hola.com/imagenes/mascotas/20180504123602/bichon-maltes-evitar-agresividad/0-563-608/bichon-maltes-t.jpg?tx=w_1200',
+        }}
+      />*/}
+      <Image
+        source={require('./assets/maltes.png')}
+      />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'indigo',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
+});
+
+const styles2 = StyleSheet.create({
+    text:{
+      color: 'white',
+      weight: 'bolder'
+    }
+});
+
