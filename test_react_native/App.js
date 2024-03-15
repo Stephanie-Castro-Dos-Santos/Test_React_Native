54aff68e08f4ae9224f206216d77d96da3283988
Stephanie-Castro-Dos-Santos <fp.daw.ies.ciudad.escolar@gmail.com>
2024-03-15 12:04:59 +0000

    New Styles

--- test_react_native/App.js
+++ test_react_native/App.js
@@ -1,21 +1,35 @@
+/* IMPORTS */
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
- /*<View></View>*/
+
+/* VARIABLES */
+const days = [1,2,3];
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={[styles2.text]}>Hello World! I'm Queen the Bichon</Text>
+      {
+      /*<Text style={[styles2.text]}>Hello World! I'm Queen the Bichon</Text>
       <Text style={[styles2.text]}>Welcome to my app</Text>
-      {/*<Image
+      <Image
         source={{
           uri: 'https://images.hola.com/imagenes/mascotas/20180504123602/bichon-maltes-evitar-agresividad/0-563-608/bichon-maltes-t.jpg?tx=w_1200',
         }}
-      />*/}
+      />
       <Image
         source={require('./assets/maltes.png')}
-      />
+      />*/
+      }
+
+        <View style={styles.box}>
+          <Text style={styles.text}>1</Text>
+        </View>
+
+        <View style={styles.box}>
+          <Text style={styles.text}>2</Text>
+        </View>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -24,16 +38,30 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'indigo',
     alignItems: 'center',
     justifyContent: 'center',
+    gap: 20
+  },
+
+  box:{
+    backgroundColor:'#F9EDE3',
+    width: 250,
+    height: 250,
+
+    /* ALIGN */
+    justifyContent: 'center',
+    alignItems: 'center',
+
+    /* BORDER */
+    boderColor: '#9b4521',
+    borderRadius: 30,
+    borderWidth:StyleSheet.hairlineWidth,
+  },
+
+  text:{
+    fontSize: 50,
+    color: '#9b4521'
   }
 });
 
-const styles2 = StyleSheet.create({
-    text:{
-      color: 'white',
-      weight: 'bolder'
-    }
-});
 
