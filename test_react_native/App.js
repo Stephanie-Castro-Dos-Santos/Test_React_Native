65c498e9da2811fac7f9110c245648717ade5852
Stephanie-Castro-Dos-Santos <fp.daw.ies.ciudad.escolar@gmail.com>
2024-03-15 13:46:39 +0000

    New FlatList with new style and distribution

--- test_react_native/App.js
+++ test_react_native/App.js
@@ -9,19 +9,6 @@ const days = [1,2,3,4,5];
 export default function App() {
   return (
     <View style={styles.container}>
-      {
-      /*<Text style={[styles2.text]}>Hello World! I'm Queen the Bichon</Text>
-      <Text style={[styles2.text]}>Welcome to my app</Text>
-      <Image
-        source={{
-          uri: 'https://images.hola.com/imagenes/mascotas/20180504123602/bichon-maltes-evitar-agresividad/0-563-608/bichon-maltes-t.jpg?tx=w_1200',
-        }}
-      />
-      <Image
-        source={require('./assets/maltes.png')}
-      />*/
-      }
-
       <FlatList
         data={days}
         renderItem={({item})=>(
@@ -29,15 +16,9 @@ export default function App() {
             <Text style={styles.text}>{item}</Text>
           </View>
         )}
-        //horizontal
       />
 
-
-      {/*days.map((day)=>(
-        <View style={styles.box} key={day}>
-          <Text style={styles.text}>{day}</Text>
-        </View>
-      ))*/}
+      
 
       <StatusBar style="auto" />
     </View>
@@ -47,8 +28,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
     gap: 10
   },
 
