3162e02a10e6883b89389d66c357eb4f4850c8ed
Stephanie-Castro-Dos-Santos <fp.daw.ies.ciudad.escolar@gmail.com>
2024-03-18 18:16:06 +0000

    src directory

---
+++ test_react_native/src/components/core/DayListItem.js
@@ -0,0 +1,33 @@
+import{View,Text,StyleSheet} from 'react-native';
+
+export default function DayListItem({day}){
+    return(
+        <View style={styles.box}>
+            <Text style={styles.text}>{day}</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+
+    box:{
+      backgroundColor:'#F9EDE3',
+      
+      flex:1,
+      aspectRatio: 1, /* For calculating width and height */
+  
+      /* ALIGN */
+      justifyContent: 'center',
+      alignItems: 'center',
+  
+      /* BORDER */
+      boderColor: '#9b4521',
+      borderRadius: 30,
+      borderWidth:StyleSheet.hairlineWidth,
+    },
+  
+    text:{
+      fontSize: 50,
+      color: '#9b4521'
+    }
+  });
